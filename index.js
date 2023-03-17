3045e4a46ac08265ee0ce3a04c70dc23bd33632b
mohamednoor <kushow8@gmail.com>
2023-03-17 23:16:44 +0300

    assignments completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2; // multiply equals 62
+
+let random = Math.floor(Math.random() * 100) + 1; // generates a random integer between 1 and 100
+
+let num3 = 28;
+let num4 = 6;
+
+let mod = num3 % num4; // mod equals 4 (the remainder of 28 / 6)
+
+let max = Math.max(5, 10, 15, 20); // max equals 20 (the highest number in the set)
